256309e69c08d13d8c528e5ba22de6321bdfdcce
Louis Viart <louis@iteracode.fr>
2019-01-22 12:09:36 +0100

    Add empty project

---
+++ MapTeam/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MapTeam
+//
+//  Created by Louis Viart on 22/01/2019.
+//  Copyright © 2019 LouisViart. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+

3ecad0d0f9e30c9cbb8d71f107b1bf1046372e2a
Louis Viart <louis@iteracode.fr>
2019-01-23 12:29:24 +0100

    Retrieve user location and display a pin

--- MapTeam/ViewController.swift
+++ MapTeam/ViewController.swift
@@ -7,11 +7,27 @@
 //
 
 import UIKit
+import MapKit
+import CoreLocation
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDelegate {
+
+    @IBOutlet weak var mapView: MKMapView!
+    
+    var locationManager: CLLocationManager?
+    var currentLocation: CLLocation!
 
     override func viewDidLoad() {
         super.viewDidLoad()
+
+        mapView.delegate = self
+
+        locationManager = CLLocationManager()
+        locationManager?.delegate = self
+        locationManager?.desiredAccuracy = kCLLocationAccuracyNearestTenMeters
+        locationManager?.requestWhenInUseAuthorization()
+        locationManager?.startUpdatingLocation()
+
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -19,7 +35,35 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+
+        if let currentUserLocation = locations.last {
+            currentLocation = currentUserLocation
+
+            let center = CLLocationCoordinate2D(
+                latitude: currentUserLocation.coordinate.latitude,
+                longitude: currentUserLocation.coordinate.longitude
+            )
+            let region = MKCoordinateRegion(
+                center: center,
+                span: MKCoordinateSpan(latitudeDelta: 0.01, longitudeDelta: 0.01)
+            )
+
+            mapView.setRegion(region, animated: true)
+
+            let userAnnotation = MKPointAnnotation()
+
+            userAnnotation.coordinate = currentUserLocation.coordinate
+            userAnnotation.title = "It's Me"
 
 
+            mapView.addAnnotation(userAnnotation)
+
+            locationManager?.stopUpdatingLocation()
+        }
+        
+    }
 }
 
